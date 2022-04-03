105776b9b76d5ecc3ddcabcf9358a7c241ef482b
Miguel Angel Zetina Zetina <loefthur26@gmail.com>
2022-04-03 16:54:03 -0500

    Constants declaration

--- src/main.go
+++ src/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hola mundo")
+	// Constants declaration
+	const pi float64 = 3.14
+	const pi2 = 3.1415
+
+	fmt.Println("pi:", pi)
+	fmt.Println("pi2:", pi2)
 }
