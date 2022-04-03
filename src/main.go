71b0c94163210e0442a1644148403fb321df22c3
Miguel Angel Zetina Zetina <loefthur26@gmail.com>
2022-04-03 17:30:36 -0500

    Use functions

--- src/main.go
+++ src/main.go
@@ -2,60 +2,32 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-func main() {
-	// Constants declaration
-	const pi float64 = 3.14
-	const pi2 = 3.1415
-
-	fmt.Println("pi:", pi)
-	fmt.Println("pi2:", pi2)
-
-	// Variables declaration
-	number := 12
-	var number1 int = 14
-	var number2 int
-
-	fmt.Println(number, number1, number2)
-
-	// Zero values
-	var zvalue1 int
-	var zvalue2 float64
-	var zvalue3 string
-	var zvalue4 bool
-	fmt.Println(zvalue1, zvalue2, zvalue3, zvalue4)
-
-	// Rectangle
-	var high = 5
-	var width = 6
-	var result = high * width
-	fmt.Println("Rectangle area is:", result)
-
-	// Trapezoid
-	var baseA = 38.0
-	var baseB = 18.0
-	var high2 = 7.0
-	var result2 = ((baseA + baseB) / 2.0) * high2
-	fmt.Println("Trapezoid area is:", result2)
-
-	// Circle
-	var radio = 7.0
-	var result3 = math.Pi * math.Pow(radio, 2)
-	fmt.Println("Circle area is:", result3)
-
-	// Printf
-	name := "Mike"
-	age := 26
-	fmt.Printf("%s is %d years old\n", name, age)
-	fmt.Printf("%v is %v years old\n", name, age)
-
-	// Sprintf
-	message := fmt.Sprintf("%v is %v years old", name, age)
+func firstFunction(message string) {
 	fmt.Println(message)
+}
 
-	// Data type
-	fmt.Printf("name type: %T\n", name)
-	fmt.Printf("age type: %T\n", age)
+func tripleArgument(a, b int, c string) {
+	fmt.Println(a, b, c)
+}
+
+func calculateDouble(number int) int {
+	return number * 2
+}
+
+func doubleReturn(a int) (c, d int) {
+	return a, a * 2
+}
+
+func main() {
+	firstFunction("Hello World!")
+	tripleArgument(1, 2, "Mike")
+	value := calculateDouble(2)
+	fmt.Println(value)
+	value1, value2 := doubleReturn(1)
+	fmt.Println(value1, value2)
+	value3, _ := doubleReturn(1)
+	fmt.Println(value3)
+
 }
