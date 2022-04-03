4c29ad070901a26febdbfdb25442dbf0a6c9ef0a
Miguel Angel Zetina Zetina <loefthur26@gmail.com>
2022-04-03 17:11:07 -0500

    Arithmetic operators

--- src/main.go
+++ src/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	// Constants declaration
@@ -23,4 +26,23 @@ func main() {
 	var zvalue3 string
 	var zvalue4 bool
 	fmt.Println(zvalue1, zvalue2, zvalue3, zvalue4)
+
+	// Rectangle
+	var high = 5
+	var width = 6
+	var result = high * width
+	fmt.Println("Rectangle area is:", result)
+
+	// Trapezoid
+	var baseA = 38.0
+	var baseB = 18.0
+	var high2 = 7.0
+	var result2 = ((baseA + baseB) / 2.0) * high2
+	fmt.Println("Trapezoid area is:", result2)
+
+	// Circle
+	var radio = 7.0
+	var result3 = math.Pi * math.Pow(radio, 2)
+	fmt.Println("Circle area is:", result3)
+
 }
