13ae92bd6227699fd9b19489901522de192023ae
Miguel Angel Zetina Zetina <loefthur26@gmail.com>
2022-04-03 17:00:00 -0500

    Zero values

--- src/main.go
+++ src/main.go
@@ -16,4 +16,11 @@ func main() {
 	var number2 int
 
 	fmt.Println(number, number1, number2)
+
+	// Zero values
+	var zvalue1 int
+	var zvalue2 float64
+	var zvalue3 string
+	var zvalue4 bool
+	fmt.Println(zvalue1, zvalue2, zvalue3, zvalue4)
 }
